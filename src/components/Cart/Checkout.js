da8730513b45b5697844a98e8619d9cd5685360f
Rupam Sutar <rupamsutar281998@gmail.com>
2022-12-14 11:03:41 +0530

    Reading the form values

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useRef } from "react";
 import Classes from "./Checkout.module.css";
 
 const Checkout = (props) => {
-    
+    const nameInputRef = useRef();
+    const streetInputRef = useRef();
+    const postalCodeInputRef = useRef();
+    const cityInputRef = useRef();
+
+    const sumbithandler = (event) => {
+        event.preventDefault();
+
+        const enteredName = nameInputRef.current.value;
+        const enteredStreet = streetInputRef.current.value;
+        const enteredPostalCode = postalCodeInputRef.current.value;
+        const enteredCity = cityInputRef.current.value;
+    }    
     return (
-        <form>
+        <form onSubmit={sumbithandler}>
             <div className={Classes.control}>
                 <label htmlFor="name">Your Name</label>
-                <input type="text" id="name"></input>
+                <input type="text" id="name" ref={nameInputRef}></input>
             </div>
             <div className={Classes.control}>
                 <label htmlFor="street">Your Street</label>
-                <input type="text" id="street"></input>
+                <input type="text" id="street" ref={streetInputRef}></input>
             </div>
             <div className={Classes.control}>
                 <label htmlFor="postal">Your Postal Code</label>
-                <input type="text" id="postal"></input>
+                <input type="text" id="postal" ref={postalCodeInputRef}></input>
             </div>
             <div className={Classes.control}>
                 <label htmlFor="city">Your City</label>
-                <input type="text" id="City"></input>
+                <input type="text" id="City" ref={cityInputRef}></input>
             </div>
             <button type="button" onClick={props.onCancel}>Cancel</button>
             <button>Confirm</button>
